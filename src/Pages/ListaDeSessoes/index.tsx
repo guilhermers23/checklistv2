1f9d6c745782fb5a2d13d46470d1209d836ef384
GuilhermeRS <guilhermemuralha2016@gmail.com>
2025-05-19 17:15:36 -0300

    configurando tela de Sessões

--- src/Pages/ListaDeSessoes/index.tsx
+++ src/Pages/ListaDeSessoes/index.tsx
@@ -1,22 +1,29 @@
-import { useState } from "react";
+//import { useEffect, useState } from "react";
 import TableSessions from "../../Components/Tables/TableSessions";
-import { getAllSessions } from "../../API/sessionService";
-import { IDadosSessao } from "../../Interfaces/ISessions";
+//import { getAllSessions } from "../../API/sessionService";
+//import { IDadosSessao } from "../../Interfaces/ISessions";
 
 const ListaDeSessoes = () => {
-    const [sessions, setSessions] = useState<IDadosSessao[]>([]);
+    const HEAD_TABLE = [
+        "Técnico", "Grupo", "Subgrupo", "Hora de Inicio", "Hora de Término"
+    ];
+    //const [sessions, setSessions] = useState<IDadosSessao[]>([]);
 
-    const findAllSessions = async () => {
-        try {
-            const dataSessions = await getAllSessions();
-            setSessions(dataSessions.data);
-        } catch (error) {
-            console.error("Ocorreu um erro ao obter as informações! ", error)
-        }
-    };
+    // const findAllSessions = async () => {
+    //     try {
+    //         const dataSessions = await getAllSessions();
+    //         setSessions(dataSessions.data);
+    //     } catch (error) {
+    //         console.error("Ocorreu um erro ao obter as informações! ", error)
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     findAllSessions();
+    // }, [])
 
     return (
-        <TableSessions />
+        <TableSessions listaCabecalho={HEAD_TABLE} listaSessoes={[]} />
     )
 };
 
